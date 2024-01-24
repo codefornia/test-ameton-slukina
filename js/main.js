15a991ab8c65f3a46f3e87cf6207cdabc88e9497
islukina <codeforniadreamin@gmail.com>
2024-01-24 21:43:34 +0300

    add index and card page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+const questionsList = document.body.getElementsByClassName('swiper')[0];
+console.log('questions', questionsList);
+questionsList.insertAdjacentHTML("afterEnd", "<ul class='questions__style'><li className='question__style'></li><li className='question__style'></li><li className='question__style'></li><li className='question__style'></li></ul>")
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    direction: getDirection(),
+    navigation: {
+        nextEl: '.question__button_next',
+        prevEl: '.question__button_prev',
+    },
+    on: {
+        resize: function () {
+            swiper.changeDirection(getDirection());
+        },
+    },
+});
+
+function getDirection() {
+    const windowWidth = window.innerWidth;
+    const direction = window.innerWidth <= 900 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
